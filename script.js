37820d64dc4b1e14c366660eb5a24e7cf7197fec
Zion Dionisio <zndionisio.2121@gmail.com>
2023-12-10 16:47:39 +0800

    Portfolio Backend first draft

---
+++ script.js
@@ -0,0 +1,19 @@
+const aboutDrawer = document.getElementById("aboutDrawer");
+const projectDrawer = document.getElementById("projectDrawer");
+const contactDrawer = document.getElementById("contactDrawer");
+
+function openAbout() {
+  aboutDrawer.style.width = "100%";
+}
+
+function openAbout() {
+  projectDrawer.style.width = "100%";
+}
+
+function openAbout() {
+  contactDrawer.style.width = "100%";
+}
+
+function closeDrawer(drawer) {
+  document.getElementById(drawer).style.width = "0%";
+}
